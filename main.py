4ae08ff56bbd4d9a4a477ab7deb9033e69d5b032
Christian Hiland <hilandchristian112@gmail.com>
2023-04-13 18:15:49 +0000

    modified:   main.py 	modified:   out.bin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-code = bytearray()
-
 text = input("Text To Add to out.bin:\n\n").encode('ascii')
 map(bin,bytearray(text))
+textn = b"Type the following command to edit /etc/hostname using nano or vi text editor: sudo nano /etc/hostname.\n Delete the old name and setup new name.\n Next Edit the /etc/hosts file: sudo nano /etc/hosts. ... \nReboot the system to changes take effect: sudo reboot."
+
 with open("out.bin", "wb") as out_file:
   out_file.write(text)
